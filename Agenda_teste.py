a08d1a7d020cf525cbb5550de906a9221ff630d2
Palomaaihara <palomaaihara@gmail.com>
2023-10-10 19:41:45 -0300

    imprimir_usuario
    imprime usuario

--- Agenda_teste.py
+++ Agenda_teste.py
@@ -2,3 +2,17 @@ def cadastro_usuario(usuarios,nome,telefone,email):
     usuario={
 
     }
+
+
+
+def imprimir_usuario(usuario):
+    for indice, usuario in enumerate(usuario):
+        print(f"ID usuario {indice + 1}")
+        print(f"Nome {usuario['Nome']}")
+        print(f"Telefone {usuario['Telefone']}")
+        print(f"Email {usuario['Email']}")
+        print("*************************************")
+        print("\n")
+
+
+        
