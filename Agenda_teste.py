fc9f229a3b380f5321688c018e63f082041d3a5e
raimundob16 <53773141+raimundob16@users.noreply.github.com>
2023-10-10 19:55:11 -0300

    nova_agenda
    nova agenda

--- Agenda_teste.py
+++
@@ -1,9 +0,0 @@
-def cadastra_produto(usuarios,nome,telefone,email):
-    usuario={
-                'Nome' : nome,
-                'Telefone' : telefone,
-                'Email' : email,
-            }
-    usuarios.append(usuario)
-    print("Usuario cadastrado com sucesso!")
-    print("*************************************")
